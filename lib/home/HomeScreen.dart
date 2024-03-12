8668e7ac6322fc392eaf0623aef663be8be92c8d
youssef <youssefm.sead@gmail.com>
2024-03-12 19:05:25 +0200

    scan & scan successfully

--- lib/home/HomeScreen.dart
+++ lib/home/HomeScreen.dart
@@ -108,7 +108,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   List<Widget> tabs = [
-    const Scan(),
+    QRViewExample(),
     const ProfilePerson(),
     const Changepassword(),
   ];
